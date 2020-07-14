fb85930e190bf60a27851357642a9be590c375ff
cingopablo <cingopablo@gmail.com>
2020-07-14 14:51:52 +0200

    Update

--- lib/widgets/new_workout/input_text.dart
+++ lib/widgets/new_workout/input_text.dart
@@ -11,30 +11,33 @@ class InputText extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      initialValue: initialValue,
-      autofocus: false,
-      autocorrect: false,
-      enableSuggestions: false,
-      maxLength: 18,
-      onChanged: (value) => setValues(key: 'title', value: value),
-      style: Theme.of(context).textTheme.headline1,
-      decoration: InputDecoration(
-        border: InputBorder.none,
-        focusedBorder: InputBorder.none,
-        enabledBorder: InputBorder.none,
-        errorBorder: InputBorder.none,
-        disabledBorder: InputBorder.none,
-        // filled: true,
-        fillColor: Colors.white,
-        labelText: 'Name',
-        //labelStyle: Theme.of(context).textTheme.headline6,
-        // hintText: 'Workout name',
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 25),
+      child: TextFormField(
+        initialValue: initialValue,
+        autofocus: false,
+        autocorrect: false,
+        enableSuggestions: false,
+        maxLength: 18,
+        onChanged: (value) => setValues(key: 'title', value: value),
+        style: Theme.of(context).textTheme.headline1,
+        decoration: InputDecoration(
+          border: InputBorder.none,
+          focusedBorder: InputBorder.none,
+          enabledBorder: InputBorder.none,
+          errorBorder: InputBorder.none,
+          disabledBorder: InputBorder.none,
+          // filled: true,
+          fillColor: Colors.white,
+          labelText: 'Name',
+          //labelStyle: Theme.of(context).textTheme.headline6,
+          // hintText: 'Workout name',
+        ),
+        validator: (value) {
+          if (value.isEmpty) return 'Please provide a name';
+          return null;
+        },
       ),
-      validator: (value) {
-        if (value.isEmpty) return 'Please provide a name';
-        return null;
-      },
     );
   }
 }
